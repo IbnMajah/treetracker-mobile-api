43a1df0ba55f64450ab8ae70dffc0e865f965a21
MagnificentTerminator <majaboujallow@gmail.com>
2022-10-19 12:42:03 +0000

    feat: setup environment

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const connectionString = "postgresql://postgres:password@localhost:5432/treetracker_db";
+export const jwtCertificate = "YTEyMzJhZD";
+export const api_client_id = "cayor_tree_tracker_client_id";
+export const api_client_secret = "cayor_tree_tracker_client_secret";
